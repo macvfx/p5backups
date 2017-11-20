df64a170938e14cac91cd2868001515a722bde3d
macvfx <xsanx@me.com>
2017-11-20 11:00:32 -0800

    Variable quoting
    trying to quote the variable properly for the command

--- check-p5.sh
+++ check-p5.sh
@@ -3,11 +3,11 @@
 # 1. check if P5 is running, if yes, then exit
 # 2. if it is not running check for running python backup
 # 3. if backup not running restart P5
-
-if [ps aux |grep "ns[d] -u root" -eq 0]
+stat=""ps aux |grep ns[d] -u root""
+if [$stat -eq 0]
 then 
 #check for python backups
-
+echo "P5 not running"
 else
 # start p5 
 /usr/local/aw/start-server
