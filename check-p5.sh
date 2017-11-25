73a7514c7c67bf1bba34a7a679ac24c5856896ed
macvfx <xsanx@me.com>
2017-11-24 18:48:53 -0800

    grep
    grep nsd

--- check-p5.sh
+++ check-p5.sh
@@ -3,12 +3,12 @@
 # 1. check if P5 is running, if yes, then exit
 # 2. if it is not running check for running python backup
 # 3. if backup not running restart P5
-stat=""ps aux |grep ns[d] -u root""
+stat=(pgrep nsd)
 if [$stat -eq 0]
 then 
-#check for python backups
-echo "P5 not running"
-else
-# start p5 
+#start server
 /usr/local/aw/start-server
+echo "P5 now running"
+else
+echo "P5 already running"
 fi
--- p5_up.py
+++ p5_up.py
@@ -8,20 +8,19 @@ import sys
 import shutil
 import time
 
-#
+print "p5 up"
 # where does your archiware install live (no trailing slash)
 aw_path = "/usr/local/aw"
 
 # check to see if nsd is already running, if it is lets throw a flag
 nr = 0 # our flag
+print "commands.getoutput('pgrep nsd'):", commands.getoutput('pgrep nsdfoo')
 if(commands.getoutput('pgrep nsd') == ""):
 	print "P5 is not running"
 	nr = 1
 
 # if P5 is NOT running, let's restart it
-if(nr != 0):
-	
-# Last, we start up the server
+if nr != 0:
 	output = subprocess.check_output([aw_path + "/start-server"])
 	print output
 	sys.exit(0)
