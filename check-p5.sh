f80771dcd7188b17667d25818ba6ef6f8d354eb8
matx <macvfx@mac.com>
2018-08-11 14:21:00 -0400

    Changed p5 detection
    p5 detection refined, find nsd owned by root, then start server also fixed

--- check-p5.sh
+++ check-p5.sh
@@ -3,9 +3,8 @@
 # 1. check if P5 is running, if yes, then exit
 # 2. if it is not running check for running python backup
 # 3. if backup not running restart P5
-stat=(pgrep nsd)
-if [$stat -eq 0]
-then 
+stat=("pgrep -u root nsd")
+if [[ $stat != 0 ]]; then
 #start server
 /usr/local/aw/start-server
 echo "P5 now running"
